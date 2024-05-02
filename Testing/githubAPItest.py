fc98386d1d639d3087ad028cce5623ae438d609a
Zeph Van Iterson <zephvaniterson@gmail.com>
2024-05-02 10:39:05 -0400

    Get all contributors and what other repos they contribute to

--- Testing/githubAPItest.py
+++ Testing/githubAPItest.py
@@ -11,8 +11,8 @@ import datetime
 
 accessToken = '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'
 headers = {'Authorization': 'token ' + accessToken}
-repoListCSV = 'OSS4SG-Project-List.csv'
-#repoListCSV = 'testdata.csv'
+#repoListCSV = 'OSS4SG-Project-List.csv'
+repoListCSV = 'testdata.csv'
 repoInfoCSV = 'OSS4SG-Project-Info.csv'
 deletedRepos = []
 
@@ -45,36 +45,8 @@ def loadRepoInfo():
         repoInfo = list(reader)
         #skip first line for titles
         repoInfo = repoInfo[1:]
-        print(repoInfo)
 
     return repoInfo
-
-
-def getRepoInfoFromFile(repo):
-    # Get the repository information from the CSV file
-    repoName = repo[0]
-    description = repo[1]
-    readmeContent = repo[2]
-
-    # print(f"Repository: {repoName}")
-    # print(f"Description: {description}")
-    # print(f"Readme Content:\n{readmeContent}")
-
-    return repoName, description, readmeContent
-
-def getAllRepoInfo():
-   
-    repoList = None
-
-    #Make sure file opens correctly
-    with open(repoListCSV, 'r') as file:
-        
-        reader = csv.reader(file)
-        repoList = list(reader)
-        #skip first line for titles
-        repoList = repoList[1:]
-
-    return repoList
 
 def getRepoInfoFromGithub(repo):
     # Make a GET request to fetch repository information
@@ -108,9 +80,28 @@ def getRepoInfoFromGithub(repo):
         contributorsResponse = requests.get('https://api.github.com/repos/'+repo[0]+'/contributors', headers=headers)
         if contributorsResponse.status_code == 200:
             contributorsInfo = contributorsResponse.json()
-            numContributors = len(contributorsInfo)
+
+
+            while 'next' in contributorsResponse.links:
+                # Make a GET request for the next page of contributors
+                next_page_url = contributorsResponse.links['next']['url']
+                contributorsResponse = requests.get(next_page_url, headers={'Accept': 'application/vnd.github.v3+json'})
+                
+                # Check if the request was successful
+                if contributorsResponse.status_code == 200:
+                    # Parse the JSON response
+                    additional_contributors = contributorsResponse.json()
+                    
+                    # Add contributors from the next page to the list
+                    contributorsInfo.extend(additional_contributors)
+                else:
+                    print(f"Failed to fetch contributors (next page). Status code: {contributorsResponse.status_code}")
+                    break
+
             for contributor in contributorsInfo:
                 contributorList.append((contributor['login'], contributor['contributions']))
+
+            numContributors = len(contributorsInfo)
 
         else:
             print(f"Failed to fetch contributors. Status code: {contributorsResponse.status_code}")
@@ -184,6 +175,98 @@ def saveRepoInfo(repoInfo):
         writer.writerow(['Repository', 'Description', 'Readme Content', 'Topics', 'Last Contribution', 'Number of Contributors', 'Number of Stars', 'Number of Subscribers', 'Contributors'])
         writer.writerows(repoInfo)
 
+def getUserInfoFromGithub(user):
+
+    # Make a GET request to fetch user information
+    response = requests.get('https://api.github.com/users/'+user, headers=headers)
+
+    userName = user
+    email = ""
+    location = ""
+    company = ""
+    numRepos = 0
+    numFollowers = 0
+    numFollowing = 0
+    userRepos = []
+    reposContributedTo = []
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        userInfo = response.json()
+        
+        # Extract relevant information from userInfo
+
+        #Do we need any of this?
+        userName = userInfo['login']
+        email = userInfo['email']
+        location = userInfo['location']
+        company = userInfo['company']
+        numRepos = userInfo['public_repos']
+        numFollowers = userInfo['followers']
+        numFollowing = userInfo['following']
+
+        #List of repos
+        reposResponse = requests.get('https://api.github.com/users/'+user+'/repos', headers=headers)
+        if reposResponse.status_code == 200:
+            repos = reposResponse.json()
+            # Check if there are more pages of repositories
+            while 'next' in reposResponse.links:
+                # Make a GET request for the next page of repositories
+                nextPageURL = reposResponse.links['next']['url']
+                reposResponse = requests.get(nextPageURL, headers={'Accept': 'application/vnd.github.v3+json'})
+                
+                # Check if the request was successful
+                if reposResponse.status_code == 200:
+                    # Parse the JSON response
+                    nextRepos = reposResponse.json()
+                    
+                    # Add repositories from the next page to the list
+                    repos.extend(nextRepos)
+                else:
+                    print(f"Failed to fetch repository contributions (next page). Status code: {reposResponse.status_code}")
+                    break
+            
+            for repo in repos:
+                repoName = repo['name']
+                userRepos.append(repoName)
+        else:
+            print(f"Failed to fetch user's repositories (first page). Status code: {reposResponse.status_code}")
+
+        publicEventsResponse = requests.get('https://api.github.com/users/'+user+'/events/public', headers=headers)
+        if publicEventsResponse.status_code == 200:
+            publicEvents = publicEventsResponse.json()
+
+            while 'next' in publicEventsResponse.links:
+                # Make a GET request for the next page of public events
+                nextPageURL = publicEventsResponse.links['next']['url']
+                publicEventsResponse = requests.get(nextPageURL, headers={'Accept': 'application/vnd.github.v3+json'})
+                
+                # Check if the request was successful
+                if publicEventsResponse.status_code == 200:
+                    # Parse the JSON response
+                    nextPublicEvents = publicEventsResponse.json()
+                    
+                    # Add public events from the next page to the list
+                    publicEvents.extend(nextPublicEvents)
+                else:
+                    print(f"Failed to fetch public events (next page). Status code: {publicEventsResponse.status_code}")
+                    break
+
+            for event in publicEvents:
+                print(event)
+                print("\n\n\n")
+                if event:
+                    if event['type'] == 'PushEvent':
+                        repoName = event['repo']['name']
+                        pushDate = event['created_at']
+                        reposContributedTo.append((repoName, pushDate))
+
+    else:
+        print(f"Failed to fetch user information. Status code: {response.status_code}")
+
+    return reposContributedTo
+
 #Fix this later
 def trainSvm(xName, xDesc, XReadMe, y):
     # Feature extraction
@@ -205,15 +288,23 @@ def trainSvm(xName, xDesc, XReadMe, y):
 
 def main():
     print("Started")
+    repoInfo = None
 
     #Get repo info from github for all repos on list and saved to info 
-    print("do you want to update the repo info? (y/n)")
+    print("Get info from Github (y/n)")
     if input() == 'y': 
-        clearCSV(repoInfoCSV)
         repoInfo = getAllRepoInfoFromGithub()
-        saveRepoInfo(repoInfo)
 
-    print(*repoInfo, sep = "\n\n\n")
+        print("Update info (y/n)") 
+        if input() == 'y':
+            clearCSV(repoInfoCSV)
+            saveRepoInfo(repoInfo)
+    #else:
+        #repoInfo = loadRepoInfo()
+    
+    getUserInfoFromGithub('simonv3')
+
+    #print(*repoInfo, sep = "\n\n\n")
 
     
 
